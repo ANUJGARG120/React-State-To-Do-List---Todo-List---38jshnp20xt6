5b9cccc51ed6e1b33918d0b0bf2e9c0181d8d376
Anuj Kumar garg <94665437+ANUJGARG120@users.noreply.github.com>
2022-04-12 13:27:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,55 @@
-import React from "react";
-import "./../styles/App.css";
-
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
-}
+import React, { useState } from 'react';
+import './style.css'
 
 
+let nextId = 0;
+const App = () => {
+  const [value, setValue] = useState('');
+  const [list, setList] = useState([]);
+
+
+  const listHandler = () => {
+    setValue('');
+    if (value.length > 0) {
+      setList([
+        ...list,
+        {
+          id: nextId++,
+          data: value,
+        },
+      ]);
+    } else {
+      alert('data is not found');
+    }
+  };
+  const valueHandler = (e) => {
+    setValue(e.target.value);
+  };
+  const deleteHandler = (id) => {
+    let newList = list.filter((item) => item.id !== id);
+    setList(newList);
+  };
+  return (
+    <div>
+    <div className="todo">
+      <h1 className="h1">Todo List</h1>
+      <input className="yes" type="text" value={value} onChange={valueHandler} />
+      <button className="btn" onClick={listHandler}>Add</button>
+      
+    </div>
+     <div className="ui" >
+   
+        {list.map((item) => (
+         <div className='li'>
+            <li  key={item.id}>
+            {item.data}
+            <button className="dlt" onClick={() => deleteHandler(item.id)}>X</button>
+          </li>
+         </div>
+        ))}
+      
+     </div>
+    </div>
+  );
+};
 export default App;
